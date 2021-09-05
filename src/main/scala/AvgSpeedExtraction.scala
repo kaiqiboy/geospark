a7f8ae9a86040619365c5db1cadb6d77f8b48d32
kaiqiboy <543100804@qq.com>
2021-09-05 16:56:09 +0800

    Update AvgSpeedExtraction.scala

--- src/main/scala/AvgSpeedExtraction.scala
+++ src/main/scala/AvgSpeedExtraction.scala
@@ -38,7 +38,9 @@ object AvgSpeedExtraction {
           val length = coords.sliding(2).map(x => greatCircleDistance(x(0), x(1))).sum
           length / (timestamps.last - timestamps.head) * 3.6
       }
-    println(combinedRDD.collect)
+    println(combinedRDD.take(5))
+    println(s"Avg speed ${(nanoTime - t) * 1e-9} s" )
+
     sc.stop()
   }
 
